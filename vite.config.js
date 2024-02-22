5513225f6fa694332e04b325f5e0521d13bcc9e1
sana.hsu <sana6622@gmail.com>
2024-02-23 00:09:39 +0800

    deploy 5

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/vue-practice-week6/',
+  base:'vue-practice-week6/',
   plugins: [
     vue(),
   ],
